d810e11eafb17ec13dfa8cb342e5d0d000419093
nadiaskau <nadiaskau@hotmail.com>
2020-10-02 12:57:37 +0200

    Oprettet quiz-spørgsmål + funktion til at printe dem

---
+++ script/questions.js
@@ -0,0 +1,33 @@
+'use strict'; 
+
+let q1 = [['Hvad er JavaScript?', 'Programmering'],
+    ['Strikkemønster', 'Navnet på Niels´s kat', 'Programmeringssprog']];
+let c1 = q1[1][2];
+
+let q2 = [['Hvad bruger man GitHub til?', 'Udviklingsmiljøer'],
+    ['Uploade kattebilleder', 'Deling og versionstyring af projekter', 'Til at lære spansk', 'Oprette webshops']];
+let c2 = q2[1][1];
+
+let q3 = [['Hvad står HTML for?', 'Programmering'],
+    ['HyperTense Markup Language', 'High Thermostate Makeup Lama', 'HyperText Markup Language']];
+let c3 = q3[1][2];
+
+let q4 = [['Hvilken én af disse er IKKE en JavaScript datatype?', 'Programmering'],
+    ['Number', 'String', 'Boolean', 'Double']];
+let c4 = q4[1][3];
+
+let q5 = [['Hvilken én af disse er IKKE en git-command', 'Udviklingsmiljøer'],
+    ['git join .', 'git add .', 'git checkout', 'git log']];
+let c5 = q5[1][0];
+
+
+let questions = [
+    [q1, c1],
+    [q2, c2],
+    [q3, c3],
+    [q4, c4],
+    [q5, c5]
+];
+
+
+export{questions};
